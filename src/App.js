77a7989b19cedf46e4050260cd943da7ff32f2e9
Christian Henry <christian.henry92@gmail.com>
2021-04-21 09:37:55 -0400

    Start basic table outline
    Implements a (currently empty) table with a row
    per draft-picking user, and 13 columns, one for
    each block they can later pick from.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const nameHeader = <th>Doctor</th>;
+
+  const blockHeaders = shiftOptions.map((option, index) => (
+    <th>{index + 1}</th>
+  ));
+
+  const mainCells = shiftOptions.map((option, index) => (
+    <td>
+      <select></select>
+    </td>
+  ));
+
+  const rows = doctors.map((name) => (
+    <tr>
+      <td>{name}</td>
+      {mainCells}
+    </tr>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Schedule Drafting</h1>
+        <table>
+          <thead>
+            {nameHeader}
+            {blockHeaders}
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
       </header>
     </div>
   );
 }
+
+var shiftOptions = [
+  "ED",
+  "ED",
+  "ED",
+  "ED",
+  "ED",
+  "ED",
+  "ED (vacation)",
+  "ED Valley",
+  "Elective",
+  "CCU",
+  "PICU",
+  "MICU",
+  "TOX/Elective",
+];
+
+var doctors = [
+  "Aaron",
+  "Nick",
+  "Amy",
+  "Charmayne",
+  "Connor",
+  "Junaid",
+  "Josh",
+  "Haseeb",
+  "Alissa",
+  "Danielle",
+  "Patrick",
+  "Nataliya",
+];
 
 export default App;

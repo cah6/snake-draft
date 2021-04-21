6eb6a473fcfbe9199721a793a525b6cdcee62f5e
Christian Henry <christian.henry92@gmail.com>
2021-04-21 10:43:06 -0400

    Fill in select fields with all options

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const nameHeader = <th>Doctor</th>;
+  const [selections, setSelections] = useState(initSelections(doctors));
 
-  const blockHeaders = shiftOptions.map((option, index) => (
-    <th>{index + 1}</th>
-  ));
+  const rows = [];
 
-  const mainCells = shiftOptions.map((option, index) => (
-    <td>
-      <select></select>
-    </td>
-  ));
-
-  const rows = doctors.map((name) => (
-    <tr>
-      <td>{name}</td>
-      {mainCells}
-    </tr>
-  ));
+  for (let [name, selectedMap] of selections.entries()) {
+    rows.push(
+      <tr>
+        <td>{name}</td>
+        {makeSelectCells(selectedMap)}
+      </tr>
+    );
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Schedule Drafting</h1>
         <table>
-          <thead>
-            {nameHeader}
-            {blockHeaders}
-          </thead>
+          {makeHeaders(allShiftOptions.length)}
           <tbody>{rows}</tbody>
         </table>
       </header>
@@ -36,7 +28,57 @@ function App() {
   );
 }
 
-var shiftOptions = [
+function makeSelectCells(selectedForUser) {
+  const cells = [];
+
+  const selectOptions = allShiftOptions.map((optionName) =>
+    makeSelectOption(optionName)
+  );
+
+  for (let [selected, blockNumber] of selectedForUser.entries()) {
+    cells.push(
+      <td>
+        <select>
+          <option value="">--</option>
+          {selectOptions}
+        </select>
+      </td>
+    );
+  }
+
+  return cells;
+}
+
+function makeSelectOption(optionName) {
+  return <option value={optionName}>{optionName}</option>;
+}
+
+function makeHeaders(numberOfBlocks) {
+  const nameHeader = <th>Doctor</th>;
+  const blockHeaders = allShiftOptions.map((option, index) => (
+    <th>{index + 1}</th>
+  ));
+  return (
+    <thead>
+      {nameHeader}
+      {blockHeaders}
+    </thead>
+  );
+}
+
+function initSelections(names) {
+  const nameMap = new Map();
+  names.forEach((name) => nameMap.set(name, initEmptySchedules()));
+  return nameMap;
+}
+
+function initEmptySchedules() {
+  const map = new Map();
+  allShiftOptions.forEach((option, index) => map.set(index + 1, ""));
+  return map;
+}
+
+const allShiftOptions = [
   "ED",
   "ED",
   "ED",
@@ -52,7 +94,7 @@ var shiftOptions = [
   "TOX/Elective",
 ];
 
-var doctors = [
+const doctors = [
   "Aaron",
   "Nick",
   "Amy",
